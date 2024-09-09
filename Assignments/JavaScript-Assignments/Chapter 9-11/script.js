e6ca2d9f4a35977053484bf0354c7ce640e4a73e
Muzammil Usman <uraza2520@gmail.com>
2024-09-10 01:52:57 +0500

    adding of chapter 9 to 11

---
+++ Assignments/JavaScript-Assignments/Chapter 9-11/script.js
@@ -0,0 +1,166 @@
+// Question Number 1
+
+let cityInput = prompt("Enter the City Name");
+if (cityInput == "Karachi") {
+  document.write("Welcome to the City of Lights");
+}
+
+// Question Number 2
+
+var gender = prompt("Enter your Gender");
+
+if (gender == "male") {
+  document.write("Hello Sir");
+}
+if (gender == "female") {
+  document.write("Hello Maam");
+}
+
+// Question Number 3
+
+var SignalColor = prompt("Enter the Signal Color");
+
+if (SignalColor == "Red") {
+  document.write("Must Stop");
+}
+if (SignalColor == "Yellow") {
+  document.write("Ready to Move");
+}
+if (SignalColor == "Green") {
+  document.write("Move Now");
+}
+
+// Question Number 4
+
+var remainingFuel = Number(prompt("Enter the Remaining Fuel of your Car"));
+
+if (remainingFuel == 0.25) {
+  document.write("Please refill fuel in your car");
+}
+
+// Question Number 5
+
+// a
+
+var a = 4;
+if (++a == 5) {
+  alert("Given condition of variable a is true");
+}
+
+// b
+
+var b = 82;
+if (b++ === 83) {
+  alert("Given condition of variable b is true");
+}
+
+// c
+
+var c = 12;
+if (c++ === 13) {
+  alert("Conditiion 1 is true");
+}
+if (c === 13) {
+  alert("Conditiion 2 is true");
+}
+if (++c < 14) {
+  alert("Conditiion 3 is true");
+}
+if (c === 14) {
+  alert("Conditiion 4 is true");
+}
+
+// d
+
+// var materialCost = 20000;
+// var labourCost = 2000;
+// var totalCost = materialCost + labourCost;
+// if (totalCost === materialCost + labourCost) {
+//   alert("Cost Equals");
+// }
+
+// e
+
+// if (True) {
+//   alert("True");
+// }
+// if (False) {
+//   alert("False");
+// }
+
+// f
+
+// if ("Car" < "Cat") {
+//   alert("Car is smaller than cat");
+// }
+
+// Question Number 6
+
+// var urduMarks = Number(prompt("Enter Subject Urdu Marks"));
+// var englishMarks = Number(prompt("Enter Subject English Marks"));
+// var mathsMarks = Number(prompt("Enter Subject Maths Marks"));
+// var totalMarks = 300;
+// document.write("Total Marks : " + totalMarks + "<br>");
+// var obtainedMarks = urduMarks + englishMarks + mathsMarks;
+// document.write("Marks Obtained : " + obtainedMarks + "<br>");
+// var percentage = (obtainedMarks / totalMarks) * 100;
+// document.write("Percentage : " + percentage + "%" + "<br>");
+// if (percentage >= 80) {
+//   document.write("Grade : A-one" + "<br>");
+//   document.write("Remarks : Excellent" + "<br>");
+// } else if (percentage >= 70) {
+//   document.write("Grade : A" + "<br>");
+//   document.write("Remarks : Good" + "<br>");
+// } else if (percentage >= 60) {
+//   document.write("Grade : B" + "<br>");
+//   document.write("Remarks : Need to Improve" + "<br>");
+// } else if (percentage < 60) {
+//   document.write("Grade : Fail" + "<br>");
+//   document.write("Remarks : Sorry" + "<br>");
+// }
+
+// Question Number 7
+
+// var secretNumber = 5;
+// var guessNumber = Number(prompt("Guess the secret number"));
+
+// if (guessNumber == secretNumber) {
+//   document.write("Bingo ! Correct Answer" + "<br>");
+// } else if (guessNumber + 1 == secretNumber) {
+//   document.write("Close enough to the correct answer." + "<br>");
+// }
+
+// Question Number 8
+
+// var checkDivide = Number(prompt("Check the number is divided by 3"));
+// if (checkDivide % 3 == 0) {
+//   document.write("Number is Divisible" + "<br>");
+// } else {
+//   document.write("Not Dividible" + "<br>");
+// }
+
+// Question Number 9
+
+// var checkRationals = Number(prompt("Enter number"));
+// if (checkRationals % 2 == 0) {
+//   document.write("Even" + "<br>");
+// } else {
+//   document.write("Odd" + "<br>");
+// }
+
+// Question Number 10
+
+// var temp = Number(prompt("Enter the temperature"));
+// if (temp > 40) {
+//   document.write("It is to hot outside" + "<br>");
+// } else if (temp > 30) {
+//   document.write("The weather today is normal" + "<br>");
+// } else if (temp > 20) {
+//   document.write("Todays weather is cool" + "<br>");
+// } else if (temp > 10) {
+//   document.write("OMG! Today’s weather is so Cool" + "<br>");
+// }
+
+// Question Number 11
+
+// Nahi aaraha
